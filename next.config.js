c3021647b50e1f5053e0ce3c7e25e24d9554ff35
M.Osman <M.Osman@MOSMAN-IT>
2025-11-04 14:28:39 +0200

    sec9

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Optional: Add basePath if you're using a subdirectory
+  // basePath: '/your-repo-name',
+}
+
+module.exports = nextConfig
